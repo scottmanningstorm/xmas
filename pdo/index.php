49647e3253a466bfe27c6824fcd4a8ae279c06b5
Scott Manning <scottm@stormcreative.co.uk>
2015-01-19 13:42:48 +0000

    asd

--- pdo/index.php
+++ pdo/index.php
@@ -1,20 +1,92 @@
-<?php 
+<?php
 
-	ini_set('display_errors', 'on');
+class Book {
+    private $author;
+    private $title;
+    function __construct($title_in, $author_in) {
+        $this->author = $author_in;
+        $this->title  = $title_in;
+    }
+    function getAuthor() {
+        return $this->author;
+    }
+    function getTitle() {
+        return $this->title;
+    }
+    function getAuthorAndTitle() {
+      return $this->getTitle().' by '.$this->getAuthor();
+    }
+}
 
-	require('connect.php');
-	 
+class BookTitleDecorator {
+    protected $book;
+    protected $title;
+    public function __construct(Book $book_in) {
+        $this->book = $book_in;
+        $this->resetTitle();
+    }   
+    //doing this so original object is not altered
+    function resetTitle() {
+        $this->title = $this->book->getTitle();
+    }
+    function showTitle() {
+        return $this->title;
+    }
+}
 
-	$db = new Connect(); 
-	$db->connect_to_database();
+class BookTitleExclaimDecorator extends BookTitleDecorator {
+    private $btd;
+    public function __construct(BookTitleDecorator $btd_in) {
+        $this->btd = $btd_in;
+    }
+    function exclaimTitle() {
+        $this->btd->title = "!" . $this->btd->title . "!";
+    }
+}
 
-	
-	
-	//$db->Print_assoc_array("SELECT * FROM test");
- 	
+class BookTitleStarDecorator extends BookTitleDecorator {
+    private $btd;
+    public function __construct(BookTitleDecorator $btd_in) {
+        $this->btd = $btd_in;
+    }
+    function starTitle() {
+        $this->btd->title = Str_replace(" ","*",$this->btd->title);
+    }
+}
 
- 	//$db->Add_to_test_table("1", "2", "3"); 
- 	//$db->delete_from_database(4); 
+  writeln('BEGIN TESTING DECORATOR PATTERN');
+  writeln('');
 
+  $patternBook = new Book('Gamma, Helm, Johnson, and Vlissides', 'Design Patterns');
+ 
+  $decorator = new BookTitleDecorator($patternBook);
+  $starDecorator = new BookTitleStarDecorator($decorator);
+  $exclaimDecorator = new BookTitleExclaimDecorator($decorator);
+ 
+  writeln('showing title : ');
+  writeln($decorator->showTitle());
+  writeln('');
+ 
+  writeln('showing title after two exclaims added : ');
+  $exclaimDecorator->exclaimTitle();
+  $exclaimDecorator->exclaimTitle();
+  writeln($decorator->showTitle());
+  writeln('');
+ 
+  writeln('showing title after star added : ');
+  $starDecorator->starTitle();
+  writeln($decorator->showTitle());
+  writeln('');
+ 
+  writeln('showing title after reset: ');
+  writeln($decorator->resetTitle());
+  writeln($decorator->showTitle());
+  writeln('');
+
+  writeln('END TESTING DECORATOR PATTERN');
+
+  function writeln($line_in) {
+    echo $line_in."<br/>";
+  }
 
 ?>
